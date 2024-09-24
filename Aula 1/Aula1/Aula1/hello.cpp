10503e2ae34f55f01320eb09e0c744169b070f5e
LucasCarvalhoPAVIC <lucaspavic2024@gmail.com>
2024-09-24 15:08:24 -0500

    Atualizações relacionadas à primeira aula de programação paralela.

---
+++ Aula 1/Aula1/Aula1/hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+    cout << "What your name and age!" << endl;
+    cin >> name >> age;
+    cout << "Your name is: " << name << "\nand age: " << age << endl;
+
+
+
+    return 0;
+}
